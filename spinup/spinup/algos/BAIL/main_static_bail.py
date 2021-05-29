27113bd2202ec29f6fd7b435a4096dcc7d5c4ff3
Xinyue <48976716+lanyavik@users.noreply.github.com>
2021-05-29 23:21:28 +0800

    Update main_static_bail.py

--- spinup/spinup/algos/BAIL/main_static_bail.py
+++ spinup/spinup/algos/BAIL/main_static_bail.py
@@ -15,7 +15,7 @@ print('data directory', os.getcwd())
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("running on device:", device)
 
-def bail_learn(env_set="Hopper-v2", seed=0, buffer_type="FinalSAC_env_%s_1000K",
+def bail_learn(env_set="Hopper-v2", seed=0, buffer_type="FinalSAC_env_0_1000K",
 					gamma=0.99, ue_rollout=1000, augment_mc='gain',
 					ue_lr=3e-3, ue_wd=2e-2, ue_loss_k=1000, ue_train_epoch=50,
 					clip_ue=False, detect_interval=10000,
